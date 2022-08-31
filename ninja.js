5c92b431e8a1dade5731d9bf17381ccb304abaf5
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-08-31 10:39:04 -0700

    filter and map

--- ninja.js
+++ ninja.js
@@ -60,7 +60,21 @@
 
     // console.log(sortedGroceries);
 
-    const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
-    const groceryList = groceries.map( item => `<li>${item}</li>` );
+    // const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
+    // const groceryList = groceries.map( item => `<li>${item}</li>` );
 
-    console.log(groceryList);
+    // console.log(groceryList);
+
+
+    const values = [1, 2, 3, 4, 5];
+    const evens = values.filter( val => val % 2 === 0 );
+
+    const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
+    const oFoods = groceries.filter( item => item.includes("o") );
+
+
+const oddCubes = values.filter( val => val % 2 !==0 ).map( val => val**3 );
+
+console.log(evens);
+console.log(oFoods);
+console.log(oddCubes);
