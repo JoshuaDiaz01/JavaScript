9b684fd87934dc69056ccd2b4f961aa1602e49bb
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-08-31 10:35:51 -0700

    map

--- ninja.js
+++ ninja.js
@@ -49,13 +49,18 @@
 //     SuperSensei.speakWisdom();
 
 
-    const groceryList = ([
-        { "item": "carrots",           "haveIngredient": false },
-        { "item": "onions",            "haveIngredient": true  },
-        { "item": "celery",            "haveIngredient": false },
-        { "item": "cremini mushrooms", "haveIngredient": false },
-        { "item": "butter",            "haveIngredient": true  }
-      ]);
-    const sortedGroceries = [...groceryList].sort( (a, b) => a.item > b.item ? 1 : -1);
+    // const groceryList = ([
+    //     { "item": "carrots",           "haveIngredient": false },
+    //     { "item": "onions",            "haveIngredient": true  },
+    //     { "item": "celery",            "haveIngredient": false },
+    //     { "item": "cremini mushrooms", "haveIngredient": false },
+    //     { "item": "butter",            "haveIngredient": true  }
+    //   ]);
+    // const sortedGroceries = [...groceryList].sort( (a, b) => a.item > b.item ? 1 : -1);
 
-    console.log(sortedGroceries);
+    // console.log(sortedGroceries);
+
+    const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
+    const groceryList = groceries.map( item => `<li>${item}</li>` );
+
+    console.log(groceryList);
