4830b6676ffe5c7164e72db841bd667b5fa9fed0
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-09-01 10:38:15 -0700

    on click events

--- prop_it_up/src/App.js
+++ prop_it_up/src/App.js
@@ -6,7 +6,7 @@ import MyNewComponent from './components/MyNewComponent';
 function App() {
   return (
     <div className="App">
-      <MyNewComponent firstName="Jane " lastName="Doe" age="29" hairColor = "brown"/>
+      <MyNewComponent firstName="Jane " lastName="Doe" age="25" hairColor = "brown"/>
       <MyNewComponent firstName="John " lastName="Smith" age="99" hairColor = "black"/>
       <MyNewComponent firstName="Millard " lastName="Fillmore" age="50" hairColor = "brown"/>
       <MyNewComponent firstName="Maria " lastName="Smith" age="62" hairColor = "purple"/>
--- prop_it_up/src/components/MyNewComponent.js
+++ prop_it_up/src/components/MyNewComponent.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
 class MyNewComponent extends Component{
+    constructor(props){
+        super(props);
+        const {age} = this.props;
+        this.state = {
+            age: age
+        }
+    }
+    addAge = (e) => {
+        
+        this.setState({age: this.state.age + 1});
+    }
+
     render(){
+
         const {firstName, lastName, age, hairColor} = this.props
+        
         return(
             <div>
                 <h1>{firstName}, {lastName} </h1>
-                
-                <h1>{age} </h1>
+                <h1>{this.state.age} </h1>
+                <button onClick = {this.addAge }>Add age by one</button>
                 <h1>{hairColor}</h1>
                 
             </div>
         );
+        
     }
 } 
 
