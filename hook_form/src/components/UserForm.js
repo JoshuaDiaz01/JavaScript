24f13bfb09e623b1a5a462af5fce4948b730de57
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-09-01 20:13:41 -0700

    update

--- hook_form/src/components/UserForm.js
+++ hook_form/src/components/UserForm.js
@@ -3,42 +3,78 @@ import React, { useState } from  'react';
     
 const UserForm = (props) => {
     const [username, setUsername] = useState("");
+    const [usernameError, setUsernameError] = useState("")
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("")
     const [password, setPassword] = useState("");  
+    const [passwordError, setPasswordError] = useState("")
     
     const createUser = (e) => {
-        setUsername("");
-        setEmail("");
-        setPassword("");
+        setUsername(e.target.value);
+        if(e.target.value.length< 5){
+            setUsernameError("Username must be 5 Characters or longer");
+        }
+        else{
+            setUsernameError("");
+        }
+        setEmail(e.target.value);
+        if(e.target.value.length< 5){
+            setEmailError("Email must be 5 Characters or longer");
+        }
+        else{
+            setEmailError("");
+        }
+        setPassword(e.target.value);
+        if(e.target.value.length< 5){
+            setPasswordError("Password must be 5 Characters or longer");
+        }
+        else{
+            setPasswordError("");
+        }
         e.preventDefault();
+
         const newUser = { username, email, password };
         console.log("Welcome", newUser);
     };
     
     return(
         <div>
-        <form onSubmit={ createUser }>
+        <form onSubmit = {(e) => e.preventDefault()}>
             <div>
                 <label>Username: </label> 
-                <input type="text" onChange={ (e) => setUsername(e.target.value) } value = {username}/>
+                <input type="text" onChange={username}/>
+                {
+                    usernameError ?
+                    <p>{usernameError}</p>:
+                    ""
+                }
             </div>
             <div>
                 <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } value = {email} />
+                <input type="text" onChange={email} /> 
+                {
+                    emailError ?
+                    <p>{emailError}</p>:
+                    ""
+                }
+
             </div>
             <div>
                 <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } value = {password}/>
+                <input type="text" onChange={password}/>
+                {
+                    passwordError ?
+                    <p>{passwordError}</p>:
+                    ""
+                }
             </div>
             <input type="submit" value="Create User" />
         </form>
-
-        
             <h1>{username}</h1>
             <h1>{email}</h1>
             <h1>{password}</h1>
         </div>
     );
 };
-    
+
 export default UserForm;
