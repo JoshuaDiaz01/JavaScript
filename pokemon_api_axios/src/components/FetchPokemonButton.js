f460fba2da00cf42512354395781807f563f19f8
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-09-06 16:21:54 -0700

    added notes

--- pokemon_api_axios/src/components/FetchPokemonButton.js
+++ pokemon_api_axios/src/components/FetchPokemonButton.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect, useRef } from "react"
 import axios from "axios"
 
+//making useState
 const FetchPokemonButton = () => {
     const [pokemons, setPokemons] = useState([]);
     const [show, setShow] = useState (false)
     //preventing useeffect to run on page load
 
+    //axios fetch of data
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807").then(response=>
             {setPokemons(response.data.results)})
     }, []
     )
 
+    //handle show conditional rendering on click
     const handleShow = (e) => {
         if(!show){
             setShow(true)
@@ -26,6 +29,7 @@ const FetchPokemonButton = () => {
         <div>
             
             <button onClick= {handleShow} >Fetch pokemon</button>
+            {/* if show is true render the pokemon */}
             {
                 show?
                 pokemons.map((pokemon, index) => {
