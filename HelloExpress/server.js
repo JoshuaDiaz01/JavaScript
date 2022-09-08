f56f42be9e2c0c546a3e3d25c8721b4671714f65
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-09-08 12:33:41 -0700

    express initial commit

---
+++ HelloExpress/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+// req is short for request
+// res is short for response
+app.get("/api", (req, res) => {
+  res.send("Our express api server is now sending this over to the browser");
+});
+app.get("/hey", (req, res) =>{
+  res.send("this is working")
+}
+)
+const server = app.listen(port, () =>
+  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+);
+
--- installations.md
+++ installations.md
@@ -1,5 +1,17 @@
-npx create-react-app
+npx create-react-app ["project name"]
 npm i axios
+
+
 <!-- express -->
-npm install 
+npm i express
+
+<!-- to install react router dom for routes -->
 npm install react-router-dom
+<!-- to run node server -->
+node server.js
+<!-- for development server -->
+nodemon server.js
+
+
+<!-- to unwrap package.json -->
+npm install 
