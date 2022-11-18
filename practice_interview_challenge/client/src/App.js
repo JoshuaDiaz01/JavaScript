2847dd7ba486ab0dc9a3c4b92544e76e51d35049
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-11-18 12:45:36 -0800

    button complete

--- practice_interview_challenge/client/src/App.js
+++ practice_interview_challenge/client/src/App.js
@@ -2,12 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import {getErrorResponse, getUser, getUsers} from "./services";
 import {User} from './components';
+import {useState} from "react";
 
 function App() {
+  //starting state as null
+  const [users, setUsers] = useState(null);
+  const [usersError, setUsersError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGetUsersOnClick = () => {
+    setIsLoading(true);
+//get users  and set them to data thats retreived 
+    getUsers()
+      .then((data) => setUsers(data.users))
+  }
+
+
+
   return (
 <div>
-  <button type="button" >Get Users</button>
+  <button onCliick = {handleGetUsersOnClick} type="button" >Get Users</button>
 </div>
+
+
   );
 }
 
